171fb33bcdb5a9bbea20317506826890b9a7fb74
prytm <125776274+prytm@users.noreply.github.com>
2025-08-14 08:50:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -44,16 +44,12 @@ if uploaded_data is not None:
 
     full_data = calc_ytd_market_share(data_copy)
 
-    mapping_df['Merk'] = mapping_df['Merk'].str.strip()
-    mapping_df['Daerah'] = mapping_df['Daerah'].str.strip()
-    full_data['Merk'] = full_data['Merk'].str.strip()
-    full_data['Daerah'] = full_data['Daerah'].str.strip()
+    full_data = full_data.merge(
+    mapping_df[['Merk', 'Daerah', 'Segment', 'Area AP']],
+    on=['Merk', 'Daerah'],
+    how='left'
+    )
 
-    segment_map = {(row['Merk'], row['Daerah']): row['Segment'] for _, row in mapping_df.iterrows()}
-    area_ap_map = {row['Daerah']: row['Area AP'] for _, row in mapping_df.iterrows()}
-
-    full_data['Segment'] = full_data.apply(lambda x: segment_map.get((x['Merk'], x['Daerah']), None), axis=1)
-    full_data['Area AP'] = full_data['Daerah'].map(area_ap_map)
     full_data = full_data.sort_values(['Tahun', 'nbulan', 'Merk']).reset_index(drop=True)
 
     full_data = full_data[['Tahun', 'Bulan', 'Daerah', 'Pulau', 'Produsen', 'Kemasan',
